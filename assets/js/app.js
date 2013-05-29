59fe05fbaabb17089961c10725cb6d4897d3cd22
Jon Cram <jon@webignition.net>
2013-05-29 12:39:52 +0100

    Initial legal docs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,40 @@
-$(document).ready(function() {    
-    prettyPrint();
-    
-    getTwitters('tweet', { 
-        id: 'simplytestable', 
-        count: 1, 
-        enableLinks: true, 
-        ignoreReplies: true, 
-        clearContents: true,
-        template: '%text% <a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id_str%/">%time%</a>',
-        callback: function () {
-            $('#tweet').html(
-                $('#tweet ul li').html()
-            );
-            
-            $('#tweet').animate({
-                'opacity':1
-            });
+String.prototype.hashCode = function(){
+    var hash = 0, i, char;
+    if (this.length == 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+$(document).ready(function() {
+    $('body.toc .post').each(function () {
+        var headings = $('h3', this);
+        if (headings.length === 0) {
+            return;
         }
+        
+        var tocList = $('<ul class="sidebar-list" />');
+        
+        headings.each(function () {
+            var thisHeading = $(this);
+            var headingId = 'h' + thisHeading.text().hashCode();
+            var idElement = $('<span class="target" id="'+headingId+'" />');
+            
+            thisHeading.before(idElement);
+            
+            //thisHeading.attr('id', headingId);
+            
+            tocList.append($('<li><a href="#'+headingId+'">'+thisHeading.text()+'</a></li>'));
+        });
+        
+        var sidebarItem = $('<div class="well" />');
+        sidebarItem.append('<h3>In this document</h3>');
+        sidebarItem.append(tocList);
+        
+        $('.sidebar').append(sidebarItem);
+        
     });
 });
