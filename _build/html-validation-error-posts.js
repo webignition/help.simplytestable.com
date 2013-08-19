a9738a51cbb17f0911da73787ec1401b4cd59a44
Jon Cram <jon@webignition.net>
2013-08-19 12:37:06 +0100

    Add 'the for attribute of the label element must ...'

--- _build/html-validation-error-posts.js
+++ _build/html-validation-error-posts.js
@@ -729,7 +729,7 @@ fs.readFile(file, 'utf8', function(err, data) {
     var error_data = JSON.parse(data);    
     var parameter_limit = 20;
     //var parameter_limit = 2;
-    var error_limit = 25;
+    var error_limit = 26;
     var parameter_depth_limit = 4;
 
     var error_subset = error_data.slice(0, error_limit);
